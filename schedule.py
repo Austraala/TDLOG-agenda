de01bf7b1a3b2c38e3169d04e3588058f704c33a
Aaron Fargeon <aaron.fargeon@eleves.enpc.fr>
2020-10-28 16:24:33 +0100

    Fichier schedule.py

---
+++ schedule.py
@@ -0,0 +1,39 @@
+"""
+This file defines the class Task for our planning system
+
+   Jean-Loup Raymond & Benjamin Roulin & Aaron Fargeon
+   ENPC - (c) 05/10/2020
+
+"""
+
+# Imports
+import datetime
+
+
+class Schedule:
+    """
+
+    """
+
+    def __init__(self, weeks):
+        self.weeks = weeks
+
+
+
+class Week:
+    """
+
+    """
+
+    def __init__(self, days):
+        self.days = days
+
+
+
+class Day:
+    """
+
+    """
+
+    def __init__(self, five_minutes_slots):
+        self.five_minutes_slots = five_minutes_slots
