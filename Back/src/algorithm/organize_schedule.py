fce6d37f804eb9ca1c570de0b417983f529d382e
Aaron Fargeon <aaron.fargeon@eleves.enpc.fr>
2020-11-28 18:15:04 +0100

    Adding back imports due to conflict (Importing Task and Schedule to organize_schedule.py)

--- Back/src/algorithm/organize_schedule.py
+++ Back/src/algorithm/organize_schedule.py
@@ -7,7 +7,8 @@ This file defines the process through which we assign a starting date to each ta
 """
 
 # Imports
-
+from entities.task import Task, FixedTask, MobileTask
+from entities.schedule import Schedule, Week, Day
 
 
 #    PART A - NAIVE OPTIMIZATION
