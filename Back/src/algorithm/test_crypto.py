eecc02dfcdb58d1f42044b88ad29ce03e3a3220a
Aaron Fargeon <aaron.fargeon@eleves.enpc.fr>
2020-11-30 13:14:45 +0100

    Separating test_crypto (new) and test_organize_schedule

---
+++ Back/src/algorithm/test_crypto.py
@@ -0,0 +1,20 @@
+"""
+This is a pytest file. To run with pytest
+
+   Aaron Fargeon
+   Benjamin Roulin
+   ENPC - (c)
+
+"""
+# pylint: disable=E0401
+
+from .crypto import encrypt, compare
+
+PASSWORD_TEST = "password_test"
+ENCRYPTED_TEST = b'$2b$16$r0ZiH4AInwwWyX2rEodRE.0dYzPKEda8N41ZsK4lIq/UavRFfQcV6'
+
+
+def test_compare():
+    """ Tests if the compare function works correctly """
+
+    assert compare(encrypt(PASSWORD_TEST), ENCRYPTED_TEST)
--- Back/src/algorithm/test_organize_schedule.py
+++ Back/src/algorithm/test_organize_schedule.py
@@ -8,18 +8,8 @@ This is a pytest file. To run with pytest
 """
 # pylint: disable=E0401
 
-from .crypto import encrypt, compare
 from .organize_schedule import merge_time_constraints, \
     test_simultaneity  # , smooth_time_constraints, register_constraint
-
-PASSWORD_TEST = "password_test"
-ENCRYPTED_TEST = b'$2b$16$r0ZiH4AInwwWyX2rEodRE.0dYzPKEda8N41ZsK4lIq/UavRFfQcV6'
-
-
-def test_compare():
-    """ Tests if the compare function works correctly """
-
-    assert compare(encrypt(PASSWORD_TEST), ENCRYPTED_TEST)
 
 
 constraint_one_test, constraint_two_test, constraint_three_test, constraint_four_test\
