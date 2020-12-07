b00e8420dcb25f4c2390c7f7ff0450fc70f53551
Aaron Fargeon <aaron.fargeon@eleves.enpc.fr>
2020-12-07 14:02:42 +0100

    Modifying anki_request.py to display Anki model names in console

--- backend/src/anki_request.py
+++ backend/src/anki_request.py
@@ -1,28 +1,5 @@
 import requests
 
-# "action": "addNote",
-    # "version": 6,
-    # "params": {
-    #     "note": {
-    #         "deckName": "Default",
-    #         "modelName": "Basic",
-    #         "fields": {
-    #             "Front": "front content",
-    #             "Back": "back content"
-    #         },
-    #         "tags": [
-    #             "yomichan"
-    #         ],
-    #         "audio": {
-    #             "url": "https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?kanji=猫&kana=ねこ",
-    #             "filename": "yomichan_ねこ_猫.mp3",
-    #             "skipHash": "7e2c2f954ef6051373ba916f000168dc",
-    #             "fields": [
-    #                 "Front"
-    #             ]
-    #         }
-    #     }
-    # }
 
 r = requests.post('http://127.0.0.1:8765', json={
     "action": "createModel",
