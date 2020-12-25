7b03b335c5c5959cbde097b7f1dbadb84de44f95
Aaron Fargeon <aaron.fargeon@eleves.enpc.fr>
2020-12-25 16:50:21 +0100

    Fixing functions to create notes, creating functions to delete notes and clear decks in anki.py (formerly anki_deck.py, testing

--- backend/src/anki.py
+++ backend/src/anki.py
@@ -10,7 +10,32 @@ def create_deck(deck_name):
         }
     })
 
+
+def deck_names(Ids=False):
+    if Ids:
+        return requests.post('http://127.0.0.1:8765', json={"action": "deckNamesAndIds", "version": 6, }).json()
+    return requests.post('http://127.0.0.1:8765', json={"action": "deckNames", "version": 6, }).json()
+
+
+def delete_deck(decks):
+    requests.post('http://127.0.0.1:8765', json={
+        "action": "deleteDecks",
+        "version": 6,
+        "params": {
+            "decks": decks,
+            "cardsToo": True,
+        }
+    })
+
+
+def clear_deck(decks):
+    delete_deck(decks)
+    for deck in decks:
+        create_deck(deck)
+
+
 def basic_note(deck, front, back):
+    print("basic(" + deck + " ; " + front + " ; " + back + ")")
     requests.post('http://127.0.0.1:8765', json={
         "action": "addNote",
         "version": 6,
@@ -26,7 +51,9 @@ def basic_note(deck, front, back):
         }
     })
 
+
 def basic_reversed_note(deck, front, back):
+    print("reverse(" + deck + " ; " + front + " ; " + back + ")")
     requests.post('http://127.0.0.1:8765', json={
         "action": "addNote",
         "version": 6,
@@ -42,7 +69,9 @@ def basic_reversed_note(deck, front, back):
         }
     })
 
+
 def basic_optional_reversed_note(deck, front, back, add_reverse):
+    print("optreverse(" + deck + " ; " + front + " ; " + back + " ; " + add_reverse + ")")
     requests.post('http://127.0.0.1:8765', json={
         "action": "addNote",
         "version": 6,
@@ -59,7 +88,9 @@ def basic_optional_reversed_note(deck, front, back, add_reverse):
         }
     })
 
+
 def basic_typein_note(deck, front, back):
+    print("typein(" + deck + " ; " + front + " ; " + back + ")")
     requests.post('http://127.0.0.1:8765', json={
         "action": "addNote",
         "version": 6,
@@ -75,7 +106,27 @@ def basic_typein_note(deck, front, back):
         }
     })
 
-def cloze_note(deck, text):
+
+def transform_for_cloze(sentence, hidden_words):
+    new_sentence = ""
+    number = 1
+    position = 0
+    while position < len(sentence):
+        word_detected = False
+        for word in hidden_words:
+            if position + len(word) <= len(sentence) + 1 and sentence[position:position + len(word)] == word:
+                word_detected = True
+                new_sentence += "{{c" + str(number) + "::" + word + "}}"
+                number += 1
+                position += len(word)
+        if not word_detected:
+            new_sentence += sentence[position]
+            position += 1
+    return new_sentence
+
+
+def cloze_note(deck, sentence, hidden_words):
+    print("cloze(" + transform_for_cloze(sentence, hidden_words) + ")")
     requests.post('http://127.0.0.1:8765', json={
         "action": "addNote",
         "version": 6,
@@ -84,12 +135,11 @@ def cloze_note(deck, text):
                 "deckName": deck,
                 "modelName": "Cloze",
                 "fields": {
-                    "Text": text,
+                    "Text": transform_for_cloze(sentence, hidden_words),
                 }
             }
         }
     })
-
 
 
 decks_to_create = ["AnaCS", "MMC1", "PhyStat", "Optimisation"]
@@ -97,33 +147,17 @@ decks_to_create = ["AnaCS", "MMC1", "PhyStat", "Optimisation"]
 for deck_to_create in decks_to_create:
     create_deck(deck_to_create)
 
-names = requests.post('http://127.0.0.1:8765', json={
-    "action": "deckNamesAndIds",
-    "version": 6,
-})
+print(deck_names())
 
-print(names.json())
+basic_note("AnaCS", "Basic ?", "Yes, basic.")
+basic_note("AnaCS", "Still basic ?", "Yes, still basic.")
+basic_typein_note("MMC1", "Type in ?", "Yes, type in.")
+basic_reversed_note("MMC1", "Reversed ?", "Yes, reversed.")
+basic_note("MMC1", "Oh my, ...", "...I'm going to be deleted !")
+basic_note("MMC1", "Oh my god, ...", "...me too !")
+basic_optional_reversed_note("Optimisation", "Opt Reverse ?", "Yes, opt reverse.", "The reverse.")
+basic_note("PhyStat", "Oh my lord, ...", "...I'm going to be homeless !")
+cloze_note("PhyStat", "Well yes, I would like a cloze card please my dear sir, thank you !",
+           ["yes", "would", "cloze", "my dear sir"])
 
-
-
-
-# print(requests.post('http://127.0.0.1:8765', json={
-#         "action": "modelNamesAndIds",
-#         "version": 6,
-#     }).json())
-
-requests.post('http://127.0.0.1:8765', json={
-    "action": "deleteDecks",
-    "version": 6,
-    "params": {
-        "decks": decks_to_create,
-        "cardsToo": True
-    }
-})
-
-names = requests.post('http://127.0.0.1:8765', json={
-    "action": "deckNames",
-    "version": 6,
-})
-
-print(names.json())
+#clear_deck(decks_to_create)
